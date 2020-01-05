e3632948e7a211590429933ea1fc3dae1450cb77
iTenis <xiehuisheng@hotmail.com>
2020-01-05 12:33:13 +0800

    fixed bug

--- itennis-lops-backend/maintenance-platform/src/main/java/com/itennishy/lops/runMainLops.java
+++ itennis-lops-backend/maintenance-platform/src/main/java/com/itennishy/lops/runMainLops.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-runMainLops {
+public class runMainLops {
 
     public static void main(String[] args) {
         SpringApplication.run(runMainLops.class, args);
